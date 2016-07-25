599a866beaf972a3ade5082e0ddf291c5d6089fb
Stewart V. Wright <stewart@vifortech.com>
2016-07-25 14:54:49 +1000

    Moved to using the github most recent stable version of OfflineIMAP

--- email/scripts/fetch-messages.sh
+++ email/scripts/fetch-messages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Ensure the metadata directory exists
-mkdir -p $(grep '^metadata' ~/.offlineimaprc | sed 's/^.* = //')
+mkdir -p $( grep '^metadata' ~/.offlineimaprc | sed 's/^.* = //' | sed "s=~=$HOME=" )
 
 # Get the email
 offlineimap.py
